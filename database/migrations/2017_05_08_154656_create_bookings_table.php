38eb89d9742a0bc16e37c747d82b490b6deaa6e4
2EasirArafat <mokremakhatun49@gmail.com>
2017-05-08 21:56:44 +0600

    Created all tables using laravel

---
+++ database/migrations/2017_05_08_154656_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('users_id');
+            $table->date('arriving_date');
+            $table->date('leaving_date');
+            $table->integer('rooms_id');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bookings');
+    }
+}
