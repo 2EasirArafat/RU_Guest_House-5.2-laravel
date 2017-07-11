fe7fe252d45297fc759916d2eade1b03d37ccaf3
2EasirArafat <mokremakhatun49@gmail.com>
2017-07-12 02:20:15 +0600

    Container problem fixed

--- resources/views/Page/check.blade.php
+++ resources/views/Page/check.blade.php
@@ -22,7 +22,7 @@
 
    <!--Table status started-->
       
-       <div class="">
+       <div class="container">
 
         <div class="panel-heading">
                  <div class="panel-title text-center">
